6227f3f6181b80a06b465e9f3e64ff30598aaee0
Timmald Fox <aprilscout.dog@gmail.com>
2022-12-07 14:51:40 -0500

    Finished get_urls

--- revImgTests.py
+++ revImgTests.py
@@ -3,7 +3,7 @@ from reverseImageSearch import *
 
 class Tests(TestCase):
     def test_get_urls_from_img(self):
-        print(get_urls_from_img('/Users/nathanwolf/Documents/Coding/PycharmProjects/1000-words/IMG_5669.jpeg'))
+        print(get_urls_from_img('https://cdn.discordapp.com/attachments/778280061245063198/1049380510897553448/dji_fly_20221202_152940_1_1670012994987_photo.jpg'))
 
     def test_get_corpus_from_urls(self):
         print(get_corpus_from_urls(get_urls_from_img('/Users/nathanwolf/Documents/Coding/PycharmProjects/1000-words/IMG_5669.jpeg')))
--- reverseImageSearch.py
+++ reverseImageSearch.py
@@ -13,12 +13,30 @@ def get_urls_from_img(imgPath:str)  -> list[str]:
     params = {
         "engine": "google_reverse_image",
         "image_url": imgPath,
-        "api_key": "secret_api_key"
+        "api_key": "29420d534203b31c11a7a15289d8e86c80dff57fde091c4600b6a4a31136d6e7"
     }
     search = GoogleSearch(params)
     results = search.get_dict()
-    # TODO: serpapi.com/manage-api-key to resolve api key issues
-    return results["urls"]
+    # How to make urlList:
+    urlList = []
+    # 1. look through the knowledge_graph dict keys
+    knowledge_graph = results['knowledge_graph']
+    for key in knowledge_graph.keys():
+        # 2. If any key has 'link' with no s, add it to urlList
+        if 'link'in key and not key[len(key) - 1] == 's':
+            urlList.append(knowledge_graph[key])
+        # 3. If any key has 'links' loop through its value and extract the 'link' from each to add to list
+        elif 'link' in key:
+            for linkDict in knowledge_graph[key]:
+                link = linkDict['link']
+                urlList.append(link)
+        # 4. If any key is has 'source' extract it's value's 'link' value and add to list
+        elif 'source' in key:
+            link = knowledge_graph[key]['link']
+            urlList.append(link)
+    return urlList
+    #  TODO: In future, pass 'description' and 'title' and 'type' values to get_corpus to add more important words.
+
 
 
 def get_corpus_from_urls(urlList:list[str]) -> str:
